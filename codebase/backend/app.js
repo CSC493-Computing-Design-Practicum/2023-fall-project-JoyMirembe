9e00ca5aa101ab037a3a5a9fdb8cc87c593cfbde
JoyMirembe <mirembej@berea.edu>
2023-09-27 10:33:19 -0400

    something

--- codebase/backend/app.js
+++ codebase/backend/app.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const cors = require('cors');
-<<<<<<< HEAD
-=======
 const { db } = require('./db/db');
 const {readdirSync} = require('fs')
->>>>>>> 85a3ace2bae50cb49c8232d14c565fcd75382a9f
 const app = express()
 
 require('dotenv').config()
@@ -15,11 +12,6 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
-<<<<<<< HEAD
-const server = () => {
-    app.listen(PORT, () => {
-        console.log
-=======
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
@@ -27,7 +19,6 @@ const server = () => {
     db()
     app.listen(PORT, () => {
         console.log('listening to port:', PORT)
->>>>>>> 85a3ace2bae50cb49c8232d14c565fcd75382a9f
     })
 }
 
